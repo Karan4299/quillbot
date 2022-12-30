eb8e8b37569805070a34d491509e75ae805c3cee
Karan Bairagi <karanbairagi@Karans-MBP-2.bbrouter>
2022-12-31 02:55:34 +0530

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
